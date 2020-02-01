95f0dbfea4044e582c4ae3b7b0b6483643b60fbf
haoyusu <haoyusu666@gmail.com>
2020-02-01 11:24:25 -0800

    fixed loop

--- src/wrangled_data.py
+++ src/wrangled_data.py
@@ -28,26 +28,18 @@ def main(input, output):
     df = df.replace('?', np.nan)
     
     # change tumor size from categorical variable to numerical values by taking the average size
-    tumor_size_str_lst = [i.split('-', 1) for i in list(df["tumor_size"])]
-    tumor_size_int_lst = []
-    tumor_size_avg_lst = []
-    for i in range(len(tumor_size_str_lst)):
-        tumor_size_int_lst.append([int(j) for j in tumor_size_str_lst[i]])
-    for i in range(len(tumor_size_int_lst)):
-        tumor_size_avg_lst.append(sum(tumor_size_int_lst[i])/len(tumor_size_int_lst[i]))
-    df["avg_tumor_size"] = tumor_size_avg_lst
-    df = df.drop(columns = ["tumor_size"])
+    df['min_tumor_size'], df['max_tumor_size'] = df['tumor_size'].str.split('-', 1).str
+    df['min_tumor_size'] = pd.to_numeric(df['min_tumor_size'])
+    df['max_tumor_size'] = pd.to_numeric(df['max_tumor_size'])
+    df["avg_tumor_size"] = (df['min_tumor_size']+df['max_tumor_size'])/2
+    df = df.drop(columns = ["tumor_size", "min_tumor_size", "max_tumor_size"])
     
     # change inv_node from categorical variable to numerical values by taking the average 
-    inv_nodes_str_lst = [i.split('-', 1) for i in list(df["inv_nodes"])]
-    inv_nodes_int_lst = []
-    inv_nodes_avg_lst = []
-    for i in range(len(inv_nodes_str_lst)):
-        inv_nodes_int_lst.append([int(j) for j in inv_nodes_str_lst[i]])
-    for i in range(len(inv_nodes_int_lst)):
-        inv_nodes_avg_lst.append(sum(inv_nodes_int_lst[i])/len(inv_nodes_int_lst[i]))
-    df["avg_inv_nodes"] = inv_nodes_avg_lst
-    df = df.drop(columns = ["inv_nodes"])
+    df['min_inv_nodes'], df['max_inv_nodes'] = df['inv_nodes'].str.split('-', 1).str
+    df['min_inv_nodes'] = pd.to_numeric(df['min_inv_nodes'])
+    df['max_inv_nodes'] = pd.to_numeric(df['max_inv_nodes'])
+    df["avg_inv_nodes"] = (df['min_inv_nodes']+df['max_inv_nodes'])/2
+    df = df.drop(columns = ["inv_nodes", "min_inv_nodes", "max_inv_nodes"])
     
     # Drop missing values
     df = df.dropna()
